a4428b008adbbfd80aa86bd59df603de4d73ceac
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2020-11-17 10:05:36 -0300

    Adding collapsable navbar, configuring nav-items;

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,28 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../_services/session.service';
 import { User } from './_models/user';
 import { LoginService } from '../_services/login.service';
-import { NavTabService } from './_services/nav-tab.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   accountModel: User = SessionService.getUser();
-  defaultRedirectUrl = SessionService.getDefaultHomePageUrl();
   barIsCollapsed = false;
   barIsExpanding = false;
   activeNavId: string;
-  currTabs: {
-    nestedTabName?: {}
-  } = {};
 
   constructor(private loginService: LoginService) { }
 
-  ngOnInit(): void {
-    this.currTabs = this.getTabsByUserType();
-  }
 
   logout() {
     this.loginService.logout();
@@ -43,17 +35,5 @@ export class HomeComponent implements OnInit {
     }
 
     this.barIsCollapsed = newCollapsedValue;
-  }
-
-  private getTabsByUserType() {
-    // switch (this.accountModel.role) {
-    //   case 'ADMIN': return {
-    //     ordersNav: NavTabService.getNavTab('ordersNav'),
-    //   };
-    //   default: return null;
-    // }
-    return {
-      nestedTabName: NavTabService.getNavTab('nestedTabName')
-    }
   }
 }
