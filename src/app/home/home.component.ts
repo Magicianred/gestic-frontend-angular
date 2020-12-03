8a186eb3bd2757447b03edf391e7dfc117bcd66f
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2020-12-03 14:46:52 -0300

    Replacing side-bar with top-nav-bar; Adding Dashboard's list of buttons;

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SessionService } from '../_services/session.service';
 import { User } from './_models/user';
 import { LoginService } from '../_services/login.service';
@@ -10,30 +10,11 @@ import { LoginService } from '../_services/login.service';
 })
 export class HomeComponent {
   accountModel: User = SessionService.getUser();
-  barIsCollapsed = false;
-  barIsExpanding = false;
-  activeNavId: string;
+  defaultRedirectUrl = SessionService.getDefaultHomePageUrl();
 
   constructor(private loginService: LoginService) { }
 
-
   logout() {
     this.loginService.logout();
-  }
-
-  toggleNavbar(forceValue?: boolean) {
-    const newCollapsedValue = (forceValue !== undefined) ? forceValue : !this.barIsCollapsed;
-
-    // Create delay for a smooth animation
-    if (this.barIsCollapsed && !newCollapsedValue) {
-      this.barIsExpanding = true;
-      setTimeout(() => { this.barIsExpanding = false; }, 100);
-    }
-    // Remove active status
-    if (newCollapsedValue) {
-      this.activeNavId = null;
-    }
-
-    this.barIsCollapsed = newCollapsedValue;
   }
 }
