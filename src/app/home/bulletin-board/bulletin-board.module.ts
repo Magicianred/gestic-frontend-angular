11894cf4ea0029292d1a36a5b45e14ddb1ef4d3a
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2020-12-09 21:45:21 -0300

    Adding & styling list-of-documents on bulletin-board component; Using mock data;

--- src/app/home/bulletin-board/bulletin-board.module.ts
+++ src/app/home/bulletin-board/bulletin-board.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BulletinBoardComponent } from './bulletin-board/bulletin-board.component';
+import { SharedModule } from '../shared.module';
 
 
 
 @NgModule({
   declarations: [BulletinBoardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class BulletinBoardModule { }
--- src/app/home/bulletin-board/bulletin-board/bulletin-board.component.ts
+++ src/app/home/bulletin-board/bulletin-board/bulletin-board.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bulletin-board.component.scss']
 })
 export class BulletinBoardComponent implements OnInit {
+  dataFilesList: {
+    name: string;
+    url: string;
+    creationDate: Date;
+  }[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataFilesList = [
+      {
+        name: 'Avaliação TCC - 1 Orientador',
+        url: 'http://www.ufal.edu.br/unidadeacademica/ic/graduacao/ciencia-da-computacao/documentos/avaliacao-tcc-1-orientador/at_download/file',
+        creationDate: new Date()
+      },
+      {
+        name: 'Avaliação TCC - 2 Orientadores',
+        url: 'http://www.ufal.edu.br/unidadeacademica/ic/graduacao/ciencia-da-computacao/documentos/avaliacao-tcc-2-orientadores/at_download/file',
+        creationDate: new Date()
+      },
+      {
+        name: 'Aproveitamento de Estudo',
+        url: 'http://www.ufal.edu.br/unidadeacademica/ic/graduacao/ciencia-da-computacao/documentos/aproveitamento-de-estudo/image_view_fullscreen',
+        creationDate: new Date()
+      }
+    ]
   }
 
 }
