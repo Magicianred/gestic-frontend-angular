8572baed4fe57a4ed0b449f0e16744b1efd37b0a
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2020-12-09 21:55:49 -0300

    Creating default-list-of-items global style; Disabling broken links;

--- src/app/home/bulletin-board/bulletin-board.module.ts
+++ src/app/home/bulletin-board/bulletin-board.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BulletinBoardComponent } from './bulletin-board/bulletin-board.component';
+import { BulletinBoardComponent } from './bulletin-board.component';
 import { SharedModule } from '../shared.module';
 
 
